4a9c3441a098922eb2b756345baf1c4fa64777bc
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-13 15:21:19 -0300

    Creando el Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import { Container, Grid, Typography } from "@mui/material"
 import Formulario from "./components/Formulario"
+import { NoticiasProvider } from "./context/NoticiasProvider"
 
 function App() {
 
   return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} variant="h3" component="h1">
-          Buscador de Noticias
-        </Typography>
-      </header>
-
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} md={6}>
-          <Formulario />
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} variant="h3" component="h1">
+            Buscador de Noticias
+          </Typography>
+        </header>
+      
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6}>
+            <Formulario />
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </NoticiasProvider>
   )
 }
 
---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react"
+
+const NoticiasContext = createContext()
+
+export const NoticiasProvider = ({ children }) => {
+
+  return (
+    <NoticiasContext.Provider value={{
+
+    }}>
+      { children }
+    </NoticiasContext.Provider>
+  )
+}
+
+export default NoticiasContext
