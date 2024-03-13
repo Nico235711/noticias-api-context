f3a32c70db2333b08f43725e6ca1917da5d8a1b6
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-13 15:50:32 -0300

    Consultando las Noticias según la categoría seleccionada

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,10 +1,24 @@
-import { createContext, useState } from "react"
+import axios from "axios"
+import { createContext, useEffect, useState } from "react"
 
 const NoticiasContext = createContext()
 
 export const NoticiasProvider = ({ children }) => {
 
   const [categoria, setCategoria] = useState("general")
+  const [noticias, setNoticias] = useState([])
+
+  useEffect(() => {
+    
+    const consultarAPI = async () => {
+      const apiKey = import.meta.env.VITE_API_KEY
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${apiKey}`
+
+      const { data } = await axios(url)
+      setNoticias(data.articles)
+    }
+    consultarAPI()
+  }, [categoria]);
 
   const handleChangeCategoria = e => {  
     setCategoria(e.target.value)
